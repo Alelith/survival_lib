9319e22ffd7314d6592a852bd2a112d1492b83c7
Lilith Estévez Boeta <101436695+Alelith@users.noreply.github.com>
2025-08-20 18:15:26 +0200

    Update print_fomat.h

--- src/printf/internal/print_fomat.h
+++ src/printf/internal/print_fomat.h
@@ -12,31 +12,5 @@
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-# include <stddef.h>
-
-int				ft_printf(char const *str, ...);
-int				ft_printdec(double num, char type);
-int				ft_printchar(char c);
-int				ft_printstr(char *str);
-int				ft_printnbr(int nbr, char type);
-int				ft_printhex(unsigned long hex, char type);
-int				ft_putdouble_fd(double d, int fd);
-int				ft_putoct_fd(unsigned int n, int fd);
-int				ft_putexp_fd(int e, int fd, int upp);
-int				ft_putnbr_fd(int n, int fd);
-int				ft_putchar_fd(char c, int fd);
-int				ft_putstr_fd(char *s, int fd);
-int				ft_putunbr_fd(unsigned int n, int fd);
-int				ft_puthex_fd(unsigned long p, int fd, int upp);
-int				ft_isflag(char c);
-int				ft_isdigit(char c);
-int				ft_isupper(char c);
-int				ft_isspecifier(char c);
-int				ft_intlen(int num);
-int				ft_octlen(unsigned int num);
-int				ft_uintlen(unsigned int num);
-int				ft_hexlen(unsigned long num);
-int				ft_atoi(const char **nptr);
-unsigned long	ft_strlen(const char *s);
 
 #endif
